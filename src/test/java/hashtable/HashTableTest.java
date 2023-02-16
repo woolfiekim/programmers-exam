57e86bdbf118f00fe213c3f1e7bfccf2db72e0f4
김주희 <woolfie@naver.com>
2023-02-16 23:27:35 +0900

    리니어 해쉬테이블

--- src/test/java/hashtable/HashTableTest.java
+++ src/test/java/hashtable/HashTableTest.java
@@ -5,13 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class HashTableTest {
 
-    @ParameterizedTest
+    @Test
     void HashTable(){
         HashTable hashTable = new HashTable(20);
         hashTable.saveData("DaveLee", "000000");
@@ -21,13 +22,23 @@ class HashTableTest {
         System.out.println(hashTable.getData("DaveLee"));
     }
 
-    @ParameterizedTest
-    void chaing(){
+    @Test
+    void Chaing(){
         Chaining chaining = new Chaining(20);
         chaining.saveData("DaveLee", "000000");
         chaining.saveData("Dead", "11111");
         chaining.saveData("hash", "22222");
         chaining.saveData("vvvv", "33333");
         System.out.println(chaining.getData("DaveLee"));
+    }
+
+    @Test
+    void Linear(){
+        Linear linear = new Linear(20);
+        linear.saveData("DaveLee", "000000");
+        linear.saveData("Dead", "11111");
+        linear.saveData("DaveLee", "22222");
+        linear.saveData("vvvv", "33333");
+        System.out.println(linear.getData("Dead"));
     }
 }
